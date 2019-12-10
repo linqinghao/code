1fd5866ab7c47d3c5edc642b92c5bfd8c6c547fd
heiyelin <heiyezoz@gmail.com>
2019-12-11 00:43:37 +0800

    refactor: update js-design-patterns

--- js-design-patterns/behavioral/pub-sub.js
+++ js-design-patterns/behavioral/pub-sub.js
@@ -1,3 +1,62 @@
 /**
  * 发布/订阅模式
  */
+
+class Publisher {
+  constructor() {
+    this.id = 0
+    this.allTopic = {}
+  }
+
+  getAllListeners(topic) {
+    return this.allTopic[topic]
+  }
+
+  subscribe(topic, f) {
+    if (!(topic in this.allTopic)) {
+      this.allTopic[topic] = []
+    }
+
+    this.allTopic[topic].push({
+      id: ++this.id,
+      func: f,
+    })
+  }
+
+  unsubscribe(topic, id) {
+    if (!(topic in this.allTopic)) return
+    this.allTopic[topic] = this.allTopic[topic].filter(item => item.id == id)
+  }
+
+  publish(topic, data) {
+    for (let subscriber of this.allTopic[topic]) {
+      subscriber.func(data)
+    }
+  }
+}
+
+let publisher = new Publisher()
+
+publisher.subscribe('change', function() {
+  console.log('change1')
+})
+
+publisher.subscribe('change', function() {
+  console.log('change2')
+})
+
+publisher.publish('change')
+
+// Output:
+// change1
+// change2
+
+let sub = publisher.subscribe('sub', function() {
+  console.log('sub')
+})
+
+console.log(publisher.getAllListeners('sub')) // Output: [ { id: 3, func: [Function] } ]
+
+publisher.unsubscribe('sub', sub)
+
+console.log(publisher.getAllListeners('sub')) // Output: []
--- js-design-patterns/behavioral/visitor.js
+++ js-design-patterns/behavioral/visitor.js
@@ -2,81 +2,84 @@
  * 访客模式
  */
 
-// class Monkey {
-//   shout() {
-//     console.log('Ooh oo aa aa!')
-//   }
+class Monkey {
+  shout() {
+    console.log('Ooh oo aa aa!')
+  }
 
-//   accept(operation) {
-//     operation.visitMonkey(this)
-//   }
-// }
+  accept(operation) {
+    operation.visitMonkey(this)
+  }
+}
 
-// class Lion {
-//   roar() {
-//     console.log('Roaaar!')
-//   }
+class Lion {
+  roar() {
+    console.log('Roaaar!')
+  }
 
-//   accept(operation) {
-//     operation.visitLion(this)
-//   }
-// }
+  accept(operation) {
+    operation.visitLion(this)
+  }
+}
 
-// class Dolphin {
-//   speak() {
-//     console.log('Tuut tuttu tuutt!')
-//   }
+class Dolphin {
+  speak() {
+    console.log('Tuut tuttu tuutt!')
+  }
 
-//   accept(operation) {
-//     operation.visitDolphin(this)
-//   }
-// }
+  accept(operation) {
+    operation.visitDolphin(this)
+  }
+}
 
-// const speak = {
-//   visitMonkey(monkey) {
-//     monkey.shout()
-//   },
-//   visitLion(lion) {
-//     lion.roar()
-//   },
-//   visitDolphin(dolphin) {
-//     dolphin.speak()
-//   },
-// }
+const speak = {
+  visitMonkey(monkey) {
+    monkey.shout()
+  },
+  visitLion(lion) {
+    lion.roar()
+  },
+  visitDolphin(dolphin) {
+    dolphin.speak()
+  },
+}
 
-// const monkey = new Monkey()
-// const lion = new Lion()
-// const dolphin = new Dolphin()
+const monkey = new Monkey()
+const lion = new Lion()
+const dolphin = new Dolphin()
 
-// monkey.accept(speak) // Ooh oo aa aa!
-// lion.accept(speak) // Roaaar!
-// dolphin.accept(speak) // Tuut tutt tuutt!
+monkey.accept(speak) // Ooh oo aa aa!
+lion.accept(speak) // Roaaar!
+dolphin.accept(speak) // Tuut tutt tuutt!
 
-// const jump = {
-//   visitMonkey(monkey) {
-//     console.log('Jumped 20 feet high! on to the tree!')
-//   },
-//   visitLion(lion) {
-//     console.log('Jumped 7 feet! Back on the ground!')
-//   },
-//   visitDolphin(dolphin) {
-//     console.log('Walked on water a little and disappeared')
-//   },
-// }
+const jump = {
+  visitMonkey(monkey) {
+    console.log('Jumped 20 feet high! on to the tree!')
+  },
+  visitLion(lion) {
+    console.log('Jumped 7 feet! Back on the ground!')
+  },
+  visitDolphin(dolphin) {
+    console.log('Walked on water a little and disappeared')
+  },
+}
 
-// monkey.accept(speak) // Ooh oo aa aa!
-// monkey.accept(jump) // Jumped 20 feet high! on to the tree!
+monkey.accept(speak) // Ooh oo aa aa!
+monkey.accept(jump) // Jumped 20 feet high! on to the tree!
 
-// lion.accept(speak) // Roaaar!
-// lion.accept(jump) // Jumped 7 feet! Back on the ground!
+lion.accept(speak) // Roaaar!
+lion.accept(jump) // Jumped 7 feet! Back on the ground!
 
-// dolphin.accept(speak) // Tuut tutt tuutt!
-// dolphin.accept(jump) // Walked on water a little and disappeared
+dolphin.accept(speak) // Tuut tutt tuutt!
+dolphin.accept(jump) // Walked on water a little and disappeared
 
+/**
+ * DEMO2
+ */
 
 function node(val) {
-  this.value = val;
-  this.left = this.right = null;
+  this.value = val
+  this.left = this.right = null
 }
 
 class Node {
@@ -85,7 +88,7 @@ class Node {
     this.left = this.right = null
   }
   accept(vistor) {
-    vistor.visit(this);
+    vistor.visit(this)
     if (this.left) this.left.accept(visitor)
     if (this.right) this.right.accept(visitor)
   }
@@ -94,32 +97,23 @@ class Node {
 let tree = new Node('A')
 tree.left = new Node('B1')
 tree.right = new Node('B2')
-tree.left.left = new Node('C1');
-tree.left.right = new Node('C2');
+tree.left.left = new Node('C1')
+tree.left.right = new Node('C2')
 
 const visitor = {
   visit(node) {
-    node.val = '*' + node.val;
+    node.val = '*' + node.val
   },
   hightlight(node) {
-    node.accept(this);
-  }
+    node.accept(this)
+  },
 }
 
-visitor.hightlight(tree.left);
+visitor.hightlight(tree.left)
 
 function dfc(node) {
-  if (node == null) return;
-  console.log(node.val);
+  if (node == null) return
+  console.log(node.val)
   dfc(node.left)
   dfc(node.right)
 }
-
-function bfc(node) {
-  if (node == null) return
-  bfc(node.left)
-  console.log(node.val);
-  bfc(node.right)
-}
-
-
