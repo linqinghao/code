d7f6048ee1953cd5326492b656cab88597c4122b
linqinghao <heiyezoz@gmail.com>
2020-07-14 17:25:31 +0800

    feat: add upload example

---
+++ example/upload/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <Upload />
+  </div>
+</template>
+
+<script>
+import Upload from './components/Upload.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld
+    Upload
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ example/upload/src/components/Upload.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <input type="file" @change="handleFileChange" />
+    <el-button type="primary" @click="hanleUpload">
+      上传
+      <i class="el-icon-upload el-icon--right"></i>
+    </el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      container: {
+        file: null,
+      },
+    }
+  },
+  methods: {
+    handleFileChange(e) {
+      let { files } = e.target;
+      if (!files) return;
+    },
+    handleUpload() {},
+  },
+}
+</script>
