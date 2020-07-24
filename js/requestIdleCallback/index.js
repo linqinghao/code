b753135b1130e53f79761463b2e2bd1fdad0ae9c
linqinghao <heiyezoz@gmail.com>
2020-07-24 11:52:13 +0800

    feat: add requestIdleCallback

---
+++ js/requestIdleCallback/index.js
@@ -0,0 +1,31 @@
+requestIdleCallback(myNonEssentialWork, { timeout: 2000 })
+
+// 任务队列
+const tasks = [
+  () => {
+    console.log('第一个任务')
+  },
+  () => {
+    console.log('第二个任务')
+  },
+  () => {
+    console.log('第三个任务')
+  },
+]
+
+function myNonEssentialWork(deadline) {
+  // 如果帧内有富余的时间，或者超时
+  while (
+    (deadline.timeRemaining() > 0 || deadline.didTimeout) &&
+    tasks.length > 0
+  ) {
+    work()
+  }
+
+  if (tasks.length > 0) requestIdleCallback(myNonEssentialWork)
+}
+
+function work() {
+  tasks.shift()()
+  console.log('执行任务')
+}
